ca5beaffb7fb221cd7736512d73cfd55121f049d
JaveriyaNooriSys <javeriya.noorisys@gmail.com>
2023-01-11 13:20:35 +0530

    seeder

--- database/seeders/SubscriptionSeeder.php
+++ database/seeders/SubscriptionSeeder.php
@@ -22,18 +22,21 @@ class SubscriptionSeeder extends Seeder
                 'subscription_type' => 'Basic',
                 'price' => 0.00,
                 'currency' => '',
+                'stripe_plan_id' => 'plan_N8VAoxvTkulA4W',
                 'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'subscription_type' => 'Premium',
                 'price' => 10.00,
                 'currency' => '',
+                'stripe_plan_id' => 'plan_N8VBy9uaXw2W6v',
                 'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'subscription_type' => 'Joint Subscription',
                 'price' => 15.00,
                 'currency' => '',
+                'stripe_plan_id' => 'plan_N8VB9tBMOsjb1a',
                 'created_at' => date('Y-m-d H:i:s')
             ]
         ]);
