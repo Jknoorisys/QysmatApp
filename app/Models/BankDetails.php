75c6d8725e117afa036bce5d67df2e18be69c1f5
JaveriyaNooriSys <javeriya.noorisys@gmail.com>
2023-01-12 17:37:06 +0530

    bank details

--- app/Models/BankDetails.php
+++ app/Models/BankDetails.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class BankDetails extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'id',
+        'user_id',
+        'user_type',
+        'card_holder_name',
+        'bank_name',
+        'card_number',
+        'month_year',
+        'cvv',
+        'status',
+        'created_at',
+        'updated_at',
+    ];
 }
