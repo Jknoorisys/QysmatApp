b26ed5d71e121e95291db6a65c148d294ea05d9d
JaveriyaNooriSys <javeriya.noorisys@gmail.com>
2023-02-27 17:21:05 +0530

    changes in swipe up

---
+++ app/Console/Commands/DeleteUserRecords.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DeleteUserRecords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:delete';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete Swiped Up profiles after 1 hour';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $one_hour_ago = now()->subHour(); 
+        DB::table('swiped_up_users')->where('created_at', '<', $one_hour_ago)->delete();
+    }
+}
