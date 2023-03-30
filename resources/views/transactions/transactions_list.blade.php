fcf378a9beb25d0c9a5543c29492136636337dfa
Javeriya <javeriya.noorisys@gmail.com>
2023-03-30 14:53:53 +0530

    testing

--- resources/views/transactions/transactions_list.blade.php
+++ resources/views/transactions/transactions_list.blade.php
@@ -152,3 +152,37 @@
     });
 
 </script>
+
+<script>
+    $(document).ready(function(){
+    
+        var from_$input = $('#from_date').pickadate(),
+        from_picker = from_$input.pickadate('picker');
+
+        var to_$input = $('#to_date').pickadate(),
+            to_picker = to_$input.pickadate('picker');
+
+
+        if (from_picker.get('value')) {
+            to_picker.set('min', from_picker.get('select'));
+        }
+        if (to_picker.get('value')) {
+            from_picker.set('max', to_picker.get('select'));
+        }
+
+        from_picker.on('set', function(event) {
+            if (event.select) {
+                to_picker.set('min', from_picker.get('select'));
+            } else if ('clear' in event) {
+                to_picker.set('min', false);
+            }
+        });
+        to_picker.on('set', function(event) {
+            if (event.select) {
+                from_picker.set('max', to_picker.get('select'));
+            } else if ('clear' in event) {
+                from_picker.set('max', false);
+            }
+        });
+});
+</script>
