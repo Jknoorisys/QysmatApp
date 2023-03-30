2e3049df1439bc75609dc3b2d21c4df96aba2c52
Javeriya <javeriya.noorisys@gmail.com>
2023-03-30 12:12:40 +0530

    in-app purchase

--- app/Http/Controllers/api/apple_pay/InAppSubscriptions.php
+++ app/Http/Controllers/api/apple_pay/InAppSubscriptions.php
@@ -747,7 +747,7 @@ class InAppSubscriptions extends Controller
                         $pdf_name = 'invoice_'.time().'.pdf';
                         $path = Storage::put('invoices/'.$pdf_name,$pdf->output());
                         $invoice_url = ('storage/app/invoices/'.$pdf_name);
-                        Transactions::where('subscription_id', '=', $originalTransactionId)->update(['invoice_url' => $invoice_url]);
+                        DB::table('transactions')->where('subscription_id', '=', $originalTransactionId)->update(['invoice_url' => $invoice_url]);
                         $email = $paymentDetails->user_email;
                         $data1 = ['salutation' => __('msg.Dear'),'name'=> $paymentDetails->user_name, 'msg'=> __('msg.This email serves to confirm the successful setup of your subscription with Us.'), 'msg1'=> __('msg.We are delighted to welcome you as a valued subscriber and are confident that you will enjoy the benefits of Premium Services.'),'msg2' => __('msg.Thank you for your trust!')];
                 
