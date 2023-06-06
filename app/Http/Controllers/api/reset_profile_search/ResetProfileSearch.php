5a19f7875a33a9128d91133227fc2d75d4b19761
Javeriya <javeriya.noorisys@gmail.com>
2023-06-06 13:58:28 +0530

    bug

--- app/Http/Controllers/api/reset_profile_search/ResetProfileSearch.php
+++ app/Http/Controllers/api/reset_profile_search/ResetProfileSearch.php
@@ -159,7 +159,8 @@ class ResetProfileSearch extends Controller
                                     Singleton::where('id', '=', $request->login_id)->update(['chat_status' => 'available']);
                 }
 
-                $other_liked = Matches::where([['match_id','=',$request->login_id],['user_type','=','singleton'], ['match_type', '=', 'liked'], ['is_reset', '=', 'yes']])->delete();
+                $other_liked = Matches::where([['match_id','=',$request->login_id],['user_type','=','singleton'], ['match_type', '=', 'liked'], ['is_reset', '=', 'yes']])
+                                        ->orWhere([['match_id','=',$request->login_id],['user_type','=','singleton'], ['match_type', '=', 'un-matched'], ['is_reset', '=', 'yes']])->delete();
 
                 Singleton::where('id', '=', $request->login_id)->update(['chat_status' => 'available']);
 
