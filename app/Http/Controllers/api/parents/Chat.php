7a1d0340ccc815ac3c40f87f1a1a8af442eca36f
Javeriya <javeriya.noorisys@gmail.com>
2023-07-10 10:46:11 +0530

    bug

--- app/Http/Controllers/api/parents/Chat.php
+++ app/Http/Controllers/api/parents/Chat.php
@@ -166,7 +166,8 @@ class Chat extends Controller
             $messaged                    = $message->save();
 
             if (!empty($messaged)) {
-                $unreadCounter = ChatHistory::where([['chat_histories.user_id', '=', $request->messaged_user_id],['chat_histories.user_type', '=', 'parent'],['chat_histories.singleton_id', '=', $request->messaged_user_singleton_id],['chat_histories.messaged_user_id', '=', $request->login_id],['chat_histories.messaged_user_type', '=', $request->user_type]])                        
+                $unreadCounter = ChatHistory::where([['chat_histories.user_id', '=', $request->messaged_user_id],['chat_histories.user_type', '=', 'parent'],['chat_histories.singleton_id', '=', $request->messaged_user_singleton_id],
+                                                    ['chat_histories.messaged_user_id', '=', $request->login_id],['chat_histories.messaged_user_type', '=', $request->user_type],['chat_histories.messaged_user_singleton_id', '=', $request->singleton_id]])                        
                                                 ->whereNull('read_at')->count();
 
                 $title = __('msg.New Message');
