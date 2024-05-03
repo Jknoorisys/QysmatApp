a023f6db78936b6557ef98f38357a5372a60631f
Javeriya <100342410+Jknoorisys@users.noreply.github.com>
2024-05-03 09:41:31 +0530

    Refactor deleteAccountDetails function to delete all matches of the deleted user

--- app/Helpers/UserExistsHelper.php
+++ app/Helpers/UserExistsHelper.php
@@ -1388,12 +1388,12 @@ use App\Notifications\MutualMatchNotification;
                                 ->update(['match_type' => 'liked', 'queue' => 0, 'is_rematched' => 'no', 'matched_at' => Null,'status' => 'available']);
                     Singleton::where('id', '=', $user_id)->orWhere('id', '=', $un_matched_id)->update(['chat_status' => 'available']);
                 }
-
-                // delete all matches of the deleted user
-                Matches::where([['user_id','=',$user_id],['user_type','=',$user_type]])
-                                ->orWhere([['match_id','=',$user_id],['user_type','=','singleton']])
-                                ->delete();
             }
+
+             // delete all matches of the deleted user
+             Matches::where([['user_id','=',$user_id],['user_type','=',$user_type]])
+                        ->orWhere([['match_id','=',$user_id],['user_type','=','singleton']])
+                        ->delete();
         } else {
             $mutual = Matches::where([['user_id','=',$user_id],['user_type','=',$user_type], ['match_type', '=', 'matched']])
                                 ->orWhere([['matched_parent_id','=',$user_id],['user_type','=','parent'], ['match_type', '=', 'matched']])
